649874a25052ac19ba2273f14798a7ae641c9499
Jaclyn <junwoo97@nate.com>
2020-12-09 23:36:03 +0900

    Modify vulnerable php source code

--- mysql.php
+++ mysql.php
@@ -4,16 +4,17 @@
     $username = getenv('MYSQL_ROOT_USER');
     $password = getenv('MYSQL_ROOT_PASSWORD');
 
+
+    echo "hi";
     $connect = mysql_connect($servername,$username,$password) or die("db error");
     mysql_select_db("hpcnt",$connect);
-    $query = "select * from hpcnt_prob where id='admin' and pw='{$_GET[pw]}'";
-    $result = mysql_query($query);
-    if(!$result) {
-	    echo 'error :'.mysql_error();
-    }
-    while( $row = mysql_fetch_array($result)) {
-	    echo 'id : '.$row[0];
-	    echo '<br>';
-	    echo 'pw :'.$row[1];
-    } 
+    $query = "select id from hpcnt_prob where id='admin' and pw='{$_GET[pw]}'";
+    echo $query;
+    $result = mysql_fetch_array(mysql_query($query)); 
+    echo $result;
+    if($result['id']) echo "<h2>Hello admin</h2>"; 
+  
+    $_GET[pw] = addslashes($_GET[pw]); 
+    $query = "select pw from hpcnt_prob where id='admin' and pw='{$_GET[pw]}'"; 
+    $result = mysql_fetch_array(mysql_query($query)); 
 ?>
